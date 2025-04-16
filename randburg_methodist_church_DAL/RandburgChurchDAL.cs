a898270e630d0bd5ef441b00712fe5a84e30f366
Mongezi Kasha <mongezikasha@gmail.com>
2025-04-16 08:05:14 +0200

    Implementing the login endpoint and auth

--- randburg_methodist_church_DAL/RandburgChurchDAL.cs
+++ randburg_methodist_church_DAL/RandburgChurchDAL.cs
@@ -5,6 +5,17 @@ namespace randburg_methodist_church_DAL
 {
     public class RandburgChurchDAL : DbContext
     {
+        public DbSet<RandMethAdmin> RandMethAdmins { get; set; }
 
+        public RandburgChurchDAL(DbContextOptions<RandburgChurchDAL> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.HasDefaultContainer("RandMethAdmin");
+            //modelBuilder.Entity<RandMethAdmin>().ToContainer("Users").HasPartitionKey(u => u.Username);
+        }
     }
 }
